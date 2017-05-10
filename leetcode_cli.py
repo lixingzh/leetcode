ddf25670fe0bda1b9be777cc1c696f418b3dee43
lixingzh <lixing.zhang@outlook.com>
2017-05-09 22:14:32 -0600

    upload leetcode_cli.py

--- leetcode_cli.py
+++ leetcode_cli.py
@@ -1,3 +1,43 @@
-#!/bin/env python
-
+#!/usr/local/bin/python3
+import json
+import subprocess
+import sys
 #script to automatically cache leetcode questions
+accepted_language=['c','cpp','csharp','golang','java','javascript','python','ruby','swift']
+
+def init_leetcode_questions(language):
+    fp = open("/Users/magicxdms/.lc/all.json")
+    leetcode_json = json.load(fp)
+
+    for i in range(len(leetcode_json)):
+        id=leetcode_json[i]['id']
+        print(id)
+        cmd = "/opt/local/bin/leetcode show %d -gx -l %s "%(id, language)
+        cmd_args=cmd.split()
+        print(cmd_args)
+        subprocess.run(cmd_args, stdout=subprocess.PIPE)
+
+def validate(code):
+    ret = False
+    for template in accepted_language:
+        if code == template:
+            ret= True
+    return ret
+
+def help_exit():
+    print ('''format:
+        leetcode_cli.py - if no language option, use default language cpp
+        leetcode_cli.py [language]
+
+        language accepted list: "c","cpp","csharp","golang","java","javascript","python","ruby","swift"''')
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        code = 'cpp'
+    else:
+        code = sys.argv[1]
+
+    if True == validate(code):
+        init_leetcode_questions(code)
+    else:
+        help_exit()
